18e8fd2e34e7910d84fc0cdf456e624cf10ec460
wei.wang <wei.wang@heweisoft.com>
2020-08-02 13:52:39 +0800

    提交代码

---
+++ src/main/java/com/biz/paic/PaicSbApplication.java
@@ -0,0 +1,13 @@
+package com.biz.paic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaicSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaicSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biz/paic/PaicSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz.paic;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaicSbApplicationTests {
+
+	@Test
+	void listUser() {
+		System.out.println("测试一下是否可以启动。。。");
+	}
+}
